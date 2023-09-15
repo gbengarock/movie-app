d4870d84bae8a65c5904d8705af68a634678aca3
gbengarock <temidayoolugbenga@yahoo.com>
2023-09-15 12:57:38 +0100

    add configuration file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-  API_KEY: process.env.SECRET_KEY || 'default_api_key',
+  API_KEY: process.env.REACT_APP_API_KEY || process.env.REACT_APP_SECRET_KEY,
   };
 
 export default config;
